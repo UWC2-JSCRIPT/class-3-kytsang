a542b8625987a52e26e8369679fe0d030b9a7455
kytsang <kytsang@uw.edu>
2022-11-07 08:40:47 -0800

    Draft HW Nov 7

--- spaceShip.js
+++ spaceShip.js
@@ -4,6 +4,27 @@
 //   `${name} moving to ${topSpeed}`
 
 
+class SpaceShip {
+    constructor(name, topSpeed){
+        this.name=name;
+        this.topSpeed=topSpeed;
+    }
+
+    accelerate() {
+        console.log(`${this.name} moving to ${this.topSpeed}`)
+    }
+}
+
+
+
+
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+const lexx= new SpaceShip("Lexx", 12000);
+const shapiro = new SpaceShip("Shapiro", "Warp Speed 5");
+
+
+lexx.accelerate();
+shapiro.accelerate();
